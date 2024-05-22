fd4107648af172fba31cfed4374525e493c5db74
Diego Viana <dtviana90@gmail.com>
2024-05-21 22:20:01 -0300

    feat: adiciona front com

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    console.log('Botão de login clicado');
+});
+
+document.getElementById('registerButton').addEventListener('click', function() {
+    console.log('Botão de registro clicado');
+});
